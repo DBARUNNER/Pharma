b5e0c31ad19866ddb92fbd9e8f7a9bb63e72493d
dbarunner <jamalyousufi2013@gmail.com>
2016-03-06 12:28:23 +0430

    migration change applied

---
+++ app/database/migrations/2016_03_06_070830_person.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Person extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('person', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('father_name');
+			$table->string('person_type');
+			$table->integer('phone');
+			$table->string('address');
+			$table->string('email');
+			$table->foreign('employee_id')
+					->references('id')
+					->on('employees');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('person');
+	}
+
+}
